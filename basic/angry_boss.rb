8d2d75e129c6b1329f672c4ddff2a7bedca2ee19
Derek Bruinooge <derek.bruinooge@gmail.com>
2021-12-19 08:48:19 +0000

    Add two basic exercises

---
+++ basic/angry_boss.rb
@@ -0,0 +1,3 @@
+puts "WHAT IS IT!?"
+input = gets.chomp
+puts "WADDAYA MEAN \"#{input.upcase}\"?!? YOU'RE FIRED!!"
---
+++ basic/table_of_contents.rb
@@ -0,0 +1,5 @@
+puts "Table of Contents".center(50)
+puts
+puts "Chapter 1: Numbers".ljust(0)   + "page 1".rjust(32)
+puts "Chapter 2: Letters".ljust(0)   + "page 5".rjust(32)
+puts "Chapter 3: Variables".ljust(0) + "page 9".rjust(30)
